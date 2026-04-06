71e64cafa800a8bad63acf6766065051a30bf594
heloizenunes21 <heloize.nunes@escola.pr.gov.br>
2026-04-06 08:16:17 -0300

    v.2

---
+++ app.js
@@ -0,0 +1,17 @@
+<p>Mensagem Inicial</p>
+//cria array 0 1 2
+let frases = ["F1", "F2", "F3"];
+let frasesCopiada = [...frases];
+//gera as frases
+function geradorMensagem(){
+if(frases.length == 0){
+frases = [...frasesCopiada];
+}
+let indiceGerado = Math.floor(Math.random()*(frases.length));
+let fraseGerada = frases [indiceGerado];
+console.log(fraseGerada);
+document.querySelector("#msg").textContent = fraseGerada;
+frases.splice(indiceGerado,1);
+
+}
+geradorMensagem();
